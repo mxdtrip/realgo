d952142857a4f4a79ef531318a5d8906d6431db1
Sfilya <Typegoot@gmail.com>
2026-08-27 13:19:37 +0300

    fix(api): check attachment close error

--- services/api/internal/reports/handler.go
+++ services/api/internal/reports/handler.go
@@ -111,10 +111,10 @@ func decodeCreateRequest(w http.ResponseWriter, r *http.Request) (Request, *Atta
 		response.Fail(w, http.StatusBadRequest, "VALIDATION_ERROR", "could not read attachment")
 		return Request{}, nil, false
 	}
-	defer file.Close()
 
 	data, err := io.ReadAll(io.LimitReader(file, MaxVideoAttachmentBytes+1))
-	if err != nil {
+	closeErr := file.Close()
+	if err != nil || closeErr != nil {
 		response.Fail(w, http.StatusBadRequest, "VALIDATION_ERROR", "could not read attachment")
 		return Request{}, nil, false
 	}
